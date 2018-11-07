cb93df019c348d81176c6121c0c06cee92546f0e
Laurancy Dorian <33289201+Laurancy-Dorian@users.noreply.github.com>
2018-11-07 13:55:36 +0100

    Added takePic function

--- Lib/cameraDriver.py
+++ Lib/cameraDriver.py
@@ -1,1 +1,16 @@
+from time import *
+from picamera import *
 
+"""
+Takes a picture form the camera and saves it in the current directory in a jpg format
+prereq :	resX > 0, resY > 0
+		 	resX <= 2592, resY <= 1944
+param :		String	filename	The name of the file
+			Int		resX 		The X resolution of the picture
+			Int 	resY		The Y resolution of the picture
+"""
+def takePic (filename ='image', resX = 1024, resY = 768):
+	camera = PiCamera()
+	camera.resolution = (resX, resY)
+	camera.start_preview()
+	camera.capture(filename + '.jpg')
