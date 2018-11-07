fec985b43a3b6c7ce1a31591892fbea4f7d36fc4
Swan ~~ <31915791+Swan-dumeige@users.noreply.github.com>
2018-11-07 15:10:09 +0100

    Update motionsensorDriver.py

--- Lib/motionsensorDriver.py
+++ Lib/motionsensorDriver.py
@@ -1,1 +1,23 @@
+import time
+import grovepi
 
+# Connect the Grove PIR Motion Sensor to digital port D8
+# SIG,NC,VCC,GND
+
+def detection():
+  pir_sensor = 8
+
+  grovepi.pinMode(pir_sensor,"INPUT")
+
+  try:
+    # Sense motion, usually human, within the target range
+    if grovepi.digitalRead(pir_sensor):
+      return(1) #Motion is detected
+    else:
+      return(0) #Motion is NOT detected  
+
+    # if your hold time is less than this, you might not see as many detections
+    time.sleep(.2)
+    
+  except IOError:
+    return("Error")
