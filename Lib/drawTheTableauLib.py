6824dbf217fdd477e3e45a99d86c1577dee34551
Etudiant IG <polig@igtp6-pc07.isim.intra>
2018-11-23 15:23:18 +0100

    Lib rm

--- Lib/drawTheTableauLib.py
+++
@@ -1,8 +0,0 @@
-"""
-Round a number up to the nearest multiple of another
-param : Int 	nb 			the number to round up
-		Int 	multiple	the multiple to round
-"""
-def roundToNearestMultiple(nb, multiple):
-	return (nb + multiple) - ((nb + multiple) % multiple)
-  
