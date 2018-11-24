9c88ce922c92bf36df522df619b32341a024d3db
Laurancy Dorian <33289201+Laurancy-Dorian@users.noreply.github.com>
2018-11-24 16:42:28 +0100

    Update drawTheTableauLib.py

--- lib/drawTheTableauLib.py
+++ lib/drawTheTableauLib.py
@@ -1,4 +1,5 @@
 import lcdDriver as lcd 
+import time as t
 
 """
 Round a number up to the nearest multiple of another
@@ -21,13 +22,20 @@ def inputDTT (msg) :
 		printDTT (msg + " (Y/N) ")
 		rep = str(raw_input (""))
 		if rep != "Y" and rep != "N" :
-			printDTT ("Erreur : Veuillez repondre \'Y\' pour oui et \'N\' pour non")
+			printDTT ("Erreur : Veuillez repondre \'Y\' pour oui et \'N\' pour non", "ERREUR : Entrez Y (oui) ou N (non)")
 			rep = "" 
 	return rep == "Y"
 
 """
 Affiche un message dans la sortie courante et sur l'ecran lcd
+Parameters :
+	- msg 		Le message a afficher
+	- msgcourt 	La version courte du message pour l'affichage sur le lcd(< 32 caracteres)
 """
-def printDTT(msg):
+def printDTT(msg, msgcourt = ""):
+	if (msgcourt = "") :
+		msgcourt = msg
 	print (msg)
 	lcd.setText(msg)
+	t.sleep(0.5)
+	
