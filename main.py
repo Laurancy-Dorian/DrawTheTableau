7f01413ab192f66cd94fa37cb38e1a9cbe3c00a3
Laurancy Dorian <33289201+Laurancy-Dorian@users.noreply.github.com>
2018-11-28 12:48:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,28 @@ import time as t
 
 if __name__ == "__main__":
     dtlib.printDTT("DrawTheTableau")
-    continuer = True
+	
+    continuer = dtlib.inputDTT ("Voulez-vous prendre une image instantanée ?", "Prendre image ?")
+
+    while continuer : 
+	dtlib.printDTT("Photo dans 5 sec")
+	t.sleep(5)
+    	takeAndSavePic()
+	dtlib.printDTT("Photo prise !")
+	continuer = dtlib.inputDTT ("Voulez-vous prendre une autre image instantanée ?", "Prendre image ?")
+	
+
+    continuer = dtlib.inputDTT ("Voulez-vous effectuer une prise auto des images ?", "Continuer prise auto img ?")
     while continuer :
 
-        print ("\n")
         dtlib.printDTT ("DEBUT DE LA PRISE AUTO D\'IMAGE")
-
         dtdriver.takeAutomatedPictures();
-	
         dtlib.printDTT ("FIN DE LA PRISE AUTO D\'IMAGE")
-        print ("\n")
 
         continuer = dtlib.inputDTT ("Voulez-vous continuer la prise auto des images ?", "Continuer prise auto img ?")
 
     rep = dtlib.inputDTT ("Voulez-vous envoyer les images sur une machine distante ?", "Envoyer sur machine distante ?")
+    #TODO
     lcd.shutDownLCD()
 
 
