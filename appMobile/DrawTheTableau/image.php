e96519089e9c79b5476fe87edf107d49f7049a77
Laurancy Dorian <33289201+Laurancy-Dorian@users.noreply.github.com>
2019-01-20 12:48:18 +0100

    Update image.php

--- appMobile/DrawTheTableau/image.php
+++ appMobile/DrawTheTableau/image.php
@@ -5,6 +5,6 @@ $folder = "/home/dodo/DrawTheTableau/img/";
 $files = scandir($folder, SCANDIR_SORT_DESCENDING);
 $newest_file = $files[0];
 copy ($folder.$newest_file, "lastImg/img.jpg");
-echo '<img style="width:500px;height:500px" src="lastImg/img.jpg"/>';
+echo '<img style="width:auto;height:100%" src="lastImg/img.jpg"/>';
 ?>
 </div>
