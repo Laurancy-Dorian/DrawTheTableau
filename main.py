c9f46303b8c1b8cb516ecf73f691b1554f0ce99e
Laurancy Dorian <33289201+Laurancy-Dorian@users.noreply.github.com>
2019-01-20 11:31:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from lib import motionsensorDriver as motion
 from lib import drawTheTableauLib as dtlib
 from lib import drawTheTableauDriver as dtdriver
 from lib import lcdDriver as lcd
+from lib import interfaceMobile as im
 import datetime as dt
 import time as t
 
@@ -12,18 +13,21 @@ if __name__ == "__main__":
     dtlib.printDTT("DrawTheTableau")
     print ("\n")
 	
-    continuer = dtlib.inputDTT ("Voulez-vous prendre une image instantanee ?", "Prendre image ?")
-
+    #continuer = dtlib.inputDTT ("Voulez-vous prendre une image instantanee ?", "Prendre image ?")
+    continuer = im.attenteReponse ("Voulez-vous prendre une image instantanee ?")
+  
     while continuer : 
 	dtlib.printDTT("Prise de photo")
     	dtdriver.takeAndSavePic()
 	dtlib.printDTT("Photo prise !")
 	print ("\n")
-	continuer = dtlib.inputDTT ("Voulez-vous prendre une autre image instantanee ?", "Prendre image ?")
+	#continuer = dtlib.inputDTT ("Voulez-vous prendre une autre image instantanee ?", "Prendre image ?")
+	continuer = im.attenteReponse ("Voulez-vous prendre une image instantanee ?")
 	
 	
 
-    continuer = dtlib.inputDTT ("Voulez-vous effectuer une prise auto des images ?", "Prise auto img ?")
+    #continuer = dtlib.inputDTT ("Voulez-vous effectuer une prise auto des images ?", "Prise auto img ?")
+    continuer = im.attenteReponse ("Voulez-vous effectuer une prise auto des images ?")
     while continuer :
 
         dtlib.printDTT ("DEBUT DE LA PRISE AUTO D\'IMAGE")
@@ -31,9 +35,10 @@ if __name__ == "__main__":
         dtlib.printDTT ("FIN DE LA PRISE AUTO D\'IMAGE")
 	print ("\n")
 
-        continuer = dtlib.inputDTT ("Voulez-vous continuer la prise auto des images ?", "Continuer prise auto img ?")
+        #continuer = dtlib.inputDTT ("Voulez-vous continuer la prise auto des images ?", "Continuer prise auto img ?")
+	continuer = im.attenteReponse ("Voulez-vous continuer la prise auto des images ?")
 
-    rep = dtlib.inputDTT ("Voulez-vous envoyer les images sur une machine distante ?", "Envoyer sur machine distante ?")
+    #rep = dtlib.inputDTT ("Voulez-vous envoyer les images sur une machine distante ?", "Envoyer sur machine distante ?")
     #TODO
     lcd.shutDownLCD()
 
