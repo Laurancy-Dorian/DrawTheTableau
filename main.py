b5bb4ab16035c10a53ab555359a774e4918d2120
Laurancy Dorian <33289201+Laurancy-Dorian@users.noreply.github.com>
2019-01-20 11:35:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     print ("\n")
 	
     #continuer = dtlib.inputDTT ("Voulez-vous prendre une image instantanee ?", "Prendre image ?")
-    continuer = im.attenteReponse ("Voulez-vous prendre une image instantanee ?")
+    continuer = im.boolattenteReponse ("Voulez-vous prendre une image instantanee ?")
   
     while continuer : 
 	dtlib.printDTT("Prise de photo")
@@ -22,12 +22,12 @@ if __name__ == "__main__":
 	dtlib.printDTT("Photo prise !")
 	print ("\n")
 	#continuer = dtlib.inputDTT ("Voulez-vous prendre une autre image instantanee ?", "Prendre image ?")
-	continuer = im.attenteReponse ("Voulez-vous prendre une image instantanee ?")
+	continuer = im.boolattenteReponse ("Voulez-vous prendre une image instantanee ?")
 	
 	
 
     #continuer = dtlib.inputDTT ("Voulez-vous effectuer une prise auto des images ?", "Prise auto img ?")
-    continuer = im.attenteReponse ("Voulez-vous effectuer une prise auto des images ?")
+    continuer = im.boolattenteReponse ("Voulez-vous effectuer une prise auto des images ?")
     while continuer :
 
         dtlib.printDTT ("DEBUT DE LA PRISE AUTO D\'IMAGE")
@@ -36,7 +36,7 @@ if __name__ == "__main__":
 	print ("\n")
 
         #continuer = dtlib.inputDTT ("Voulez-vous continuer la prise auto des images ?", "Continuer prise auto img ?")
-	continuer = im.attenteReponse ("Voulez-vous continuer la prise auto des images ?")
+	continuer = im.boolattenteReponse ("Voulez-vous continuer la prise auto des images ?")
 
     #rep = dtlib.inputDTT ("Voulez-vous envoyer les images sur une machine distante ?", "Envoyer sur machine distante ?")
     #TODO
